56cb09c8d0110b11831ce66c764460ccec42fb4d
joaomanoel <belezajovaniatb@gmail.com>
2024-03-04 00:34:01 -0300

    feat(account_repository): repository to account

---
+++ src/persistence/account_repository.rs
@@ -0,0 +1,41 @@
+use postgres::{Client, NoTls};
+
+use crate::model::account::Account;
+
+use super::database_url::url_postgres;
+
+pub struct AccountRepository;
+
+pub enum AccountRepositoryError {
+    AccountNotFound,
+    ConnectionError,
+}
+
+impl AccountRepository {
+    pub fn get_accout_by_id(&self, id: i32) -> Result<Account, AccountRepositoryError> {
+        let client = Client::connect(&url_postgres(), NoTls);
+
+        match client {
+            Ok(mut db) => {
+                let row = db.query_one(
+                    r#"
+                    SELECT clientes.limite, saldos.valor 
+                    FROM clientes
+                    LEFT JOIN saldos ON clientes.id = saldos.cliente_id
+                    WHERE cliente_id = $1;
+                "#,
+                    &[&id],
+                );
+
+                match row {
+                    Ok(account) => Ok(Account {
+                        limite: account.get("limite"),
+                        saldo: account.get("valor"),
+                    }),
+                    _ => Err(AccountRepositoryError::AccountNotFound),
+                }
+            }
+            _ => Err(AccountRepositoryError::ConnectionError),
+        }
+    }
+}
