164ced3acdaadc784de9b8a90206b54a51e9886d
joaomanoel <belezajovaniatb@gmail.com>
2024-03-04 00:39:23 -0300

    feat(transaction_repository): repository to transaction

---
+++ src/persistence/transaction_repository.rs
@@ -0,0 +1,106 @@
+use postgres::{Client, Error, NoTls};
+
+use crate::{model::transaction::Transaction, persistence::database_url::url_postgres};
+
+pub struct TransactionRepository;
+
+#[derive(Debug)]
+pub enum TransactionRepositoryError {
+    ErrorRoolback,
+    ErrorSaveTransaction,
+    ErrorDbTransaction,
+    ErrorConnectDatabase,
+}
+
+impl TransactionRepository {
+    pub fn get_transactions_by_account_id(
+        &self,
+        id: i32,
+    ) -> Result<Vec<Transaction>, TransactionRepositoryError> {
+        let client = Client::connect(&url_postgres(), NoTls);
+
+        match client {
+            Ok(mut db) => {
+                let row_transactions = db.query(
+                    r#"
+                        SELECT transacoes.valor, transacoes.tipo, transacoes.descricao, transacoes.realizada_em
+                        FROM transacoes
+                        WHERE transacoes.cliente_id = $1
+                        ORDER BY transacoes.realizada_em DESC
+                        LIMIT 10;
+                    "#,
+                    &[&id],
+                );
+
+                let mut transactions: Vec<Transaction> = Vec::new();
+
+                if let Ok(vec_transactions) = row_transactions {
+                    vec_transactions.iter().for_each(|row| {
+                        transactions.push(Transaction {
+                            valor: row.get("valor"),
+                            tipo: row.get("tipo"),
+                            descricao: row.get("descricao"),
+                            realizada_em: row.get("realizada_em"),
+                        })
+                    });
+                }
+
+                Ok(transactions)
+            }
+            _ => Err(TransactionRepositoryError::ErrorConnectDatabase),
+        }
+    }
+
+    pub fn create_transaction(
+        &self,
+        id: i32,
+        value: i32,
+        limit: i32,
+        balance: i32,
+        transaction_type: String,
+        descrition: String,
+    ) -> Result<(), TransactionRepositoryError> {
+        let client = Client::connect(&url_postgres(), NoTls);
+
+        match client {
+            Ok(mut db) => {
+                let db_transaction_result = db.transaction();
+
+                match db_transaction_result {
+                    Ok(mut db_transaction) => {
+                        let db_transaction_save = db_transaction.execute(
+                            r#"INSERT INTO transacoes (cliente_id, valor, tipo, descricao) VALUES ($1, $2, $3, $4);
+                        "#, &[&id, &value, &transaction_type, &descrition]);
+
+                        let db_saldo_update = db_transaction.execute(
+                            r#"UPDATE saldos SET valor = $1 WHERE cliente_id = $2;
+                        "#,
+                            &[&(value + balance), &id],
+                        );
+
+                        let mut db_clientes_update: Result<u64, Error> = Ok(0);
+                        if transaction_type == "c" {
+                            db_clientes_update = db_transaction.execute(
+                                r#"UPDATE clientes SET limite = $1 WHERE id = $2;"#,
+                                &[&(limit - value), &id],
+                            );
+                        }
+
+                        match (db_transaction_save, db_saldo_update, db_clientes_update) {
+                            (Ok(_), Ok(_), Ok(_)) => db_transaction.commit().map_or_else(
+                                |_| Err(TransactionRepositoryError::ErrorSaveTransaction),
+                                |_| Ok(()),
+                            ),
+                            _ => db_transaction.rollback().map_or_else(
+                                |_| Err(TransactionRepositoryError::ErrorRoolback),
+                                |_| Err(TransactionRepositoryError::ErrorSaveTransaction),
+                            ),
+                        }
+                    }
+                    _ => Err(TransactionRepositoryError::ErrorDbTransaction),
+                }
+            }
+            _ => Err(TransactionRepositoryError::ErrorConnectDatabase),
+        }
+    }
+}
