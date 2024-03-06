eb6f434d653fdc2d42fe526f5a588129e8839d71
joaomanoel <belezajovaniatb@gmail.com>
2024-03-06 00:38:43 -0300

    chore(database_url): simplified

--- src/persistence/database_url.rs
+++ src/persistence/database_url.rs
@@ -1,11 +1,5 @@
 use std::env;
 
 pub fn url_postgres() -> String {
-    format!(
-        "host={} user={} password={} dbname={}",
-        env::var("POSTGRES_HOST").unwrap(),
-        env::var("POSTGRES_USER").unwrap(),
-        env::var("POSTGRES_PASSWORD").unwrap(),
-        env::var("POSTGRES_DB").unwrap()
-    )
+    env::var("DATABASE_URL").unwrap().to_string()
 }
