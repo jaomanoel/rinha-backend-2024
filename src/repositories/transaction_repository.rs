788d1fe379c57ea24160c90cc057fc9ff802ad51
jaomanoel <belezajovaniatb@gmail.com>
2024-09-02 00:23:38 -0300

    refactor: update TransactionRepository to use shared Database connection
    - Refactored `get_transactions_by_account_id` and `create_transaction` to use the shared `Database` connection.
    - Removed PostgreSQL connection management from methods, utilizing a pre-existing database connection passed as a reference.
    - Simplified the query structure and variable naming for consistency with the new database model.
    - Updated logic for transaction creation and account balance update to match the new database schema.

--- src/repositories/transaction_repository.rs
+++ src/repositories/transaction_repository.rs
@@ -1,6 +1,6 @@
-use postgres::{Client, Error, NoTls};
+use postgres::Error;
 
-use crate::{model::transaction::Transaction, persistence::database_url::url_postgres};
+use crate::{model::transaction::Transaction, persistence::database::Database};
 
 pub struct TransactionRepository;
 
@@ -13,97 +13,72 @@ pub enum TransactionRepositoryError {
 }
 
 impl TransactionRepository {
-    pub fn get_transactions_by_account_id(
-        &self,
-        id: i32,
-    ) -> Result<Vec<Transaction>, TransactionRepositoryError> {
-        let client = Client::connect(&url_postgres(), NoTls);
+    pub fn get_transactions_by_account_id(&self, id: u32, db: &mut Database) -> Vec<Transaction> {
+        let row_transactions = db.connection.query(
+            r#"
+                SELECT amount, transaction_type, description, created
+                FROM transactions
+                WHERE account_id = $1
+                ORDER BY created DESC
+                LIMIT 10;
+            "#,
+            &[&(id as i32)],
+        );
 
-        match client {
-            Ok(mut db) => {
-                let row_transactions = db.query(
-                    r#"
-                        SELECT valor, tipo, descricao, realizada_em
-                        FROM transacoes
-                        WHERE cliente_id = $1
-                        ORDER BY realizada_em DESC
-                        LIMIT 10;
-                    "#,
-                    &[&id],
-                );
+        let mut transactions: Vec<Transaction> = Vec::new();
 
-                let mut transactions: Vec<Transaction> = Vec::new();
-
-                if let Ok(vec_transactions) = row_transactions {
-                    vec_transactions.iter().for_each(|row| {
-                        transactions.push(Transaction {
-                            valor: row.get("valor"),
-                            tipo: row.get("tipo"),
-                            descricao: row.get("descricao"),
-                            realizada_em: row.get("realizada_em"),
-                        })
-                    });
-                }
-
-                Ok(transactions)
-            }
-            _ => Err(TransactionRepositoryError::ErrorConnectDatabase),
+        if let Ok(vec_transactions) = row_transactions {
+            vec_transactions.iter().for_each(|row| {
+                transactions.push(Transaction {
+                    valor: row.get("amount"),
+                    tipo: row.get("transaction_type"),
+                    descricao: row.get("description"),
+                    realizada_em: row.get("created"),
+                })
+            });
         }
+
+        transactions
     }
 
     pub fn create_transaction(
         &self,
-        id: i32,
+        id: u32,
         value: i32,
-        limit: i32,
         balance: i32,
         transaction_type: String,
         descrition: String,
+        db: &mut Database,
     ) -> Result<(), TransactionRepositoryError> {
-        let client = Client::connect(&url_postgres(), NoTls);
+        match db.connection.transaction() {
+            Ok(mut db_transaction) => {
+                let db_transaction_save = db_transaction.execute(r#"
+                    INSERT INTO transactions (account_id, amount, transaction_type, description) VALUES ($1, $2, $3, $4);
+                "#, &[&(id as i32), &value, &transaction_type, &descrition]);
 
-        match client {
-            Ok(mut db) => {
-                let db_transaction_result = db.transaction();
+                let amount: i32 = match transaction_type.as_ref() {
+                    "c" => balance + value,
+                    "d" => balance - value,
+                    _ => 0,
+                };
 
-                match db_transaction_result {
-                    Ok(mut db_transaction) => {
-                        let db_transaction_save = db_transaction.execute(
-                            r#"INSERT INTO transacoes (cliente_id, valor, tipo, descricao) VALUES ($1, $2, $3, $4);
-                        "#, &[&id, &value, &transaction_type, &descrition]);
+                let db_clientes_update: Result<u64, Error> = db_transaction.execute(
+                    r#"UPDATE accounts SET balance = $1 WHERE accounts.id = $2;"#,
+                    &[&amount, &(id as i32)],
+                );
 
-                        let db_saldo_update = db_transaction.execute(
-                            r#"UPDATE saldos SET valor = $1 WHERE cliente_id = $2;
-                        "#,
-                            &[&(balance - value), &id],
-                        );
-
-                        let amount: i32 = match transaction_type.as_ref() {
-                            "c" => limit + value,
-                            "d" => limit - value,
-                            _ => 0,
-                        };
-
-                        let db_clientes_update: Result<u64, Error> = db_transaction.execute(
-                            r#"UPDATE clientes SET limite = $1 WHERE id = $2;"#,
-                            &[&amount, &id],
-                        );
-
-                        match (db_transaction_save, db_saldo_update, db_clientes_update) {
-                            (Ok(_), Ok(_), Ok(_)) => db_transaction.commit().map_or_else(
-                                |_| Err(TransactionRepositoryError::ErrorSaveTransaction),
-                                |_| Ok(()),
-                            ),
-                            _ => db_transaction.rollback().map_or_else(
-                                |_| Err(TransactionRepositoryError::ErrorRoolback),
-                                |_| Err(TransactionRepositoryError::ErrorSaveTransaction),
-                            ),
-                        }
-                    }
-                    _ => Err(TransactionRepositoryError::ErrorDbTransaction),
+                match (db_transaction_save, db_clientes_update) {
+                    (Ok(_), Ok(_)) => db_transaction.commit().map_or_else(
+                        |_| Err(TransactionRepositoryError::ErrorSaveTransaction),
+                        |_| Ok(()),
+                    ),
+                    _ => db_transaction.rollback().map_or_else(
+                        |_| Err(TransactionRepositoryError::ErrorRoolback),
+                        |_| Err(TransactionRepositoryError::ErrorSaveTransaction),
+                    ),
                 }
             }
-            _ => Err(TransactionRepositoryError::ErrorConnectDatabase),
+            _ => Err(TransactionRepositoryError::ErrorDbTransaction),
         }
     }
 }
