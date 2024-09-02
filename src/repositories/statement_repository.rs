ea55c4533265cb4bba1ff08aad2c2769910889f8
jaomanoel <belezajovaniatb@gmail.com>
2024-09-02 00:23:09 -0300

    refactor: update StatementRepository to utilize shared Database connection
    - Replaced direct transaction fetching with a new approach leveraging a mutable `Database` reference.
    - Simplified error handling by removing custom error types, assuming that transaction errors will be handled at a higher layer.
    - Updated transaction queries to use a shared `Database` object for better resource management and concurrency control.
    - Streamlined the method to return `StatementResponse` directly, removing unnecessary error branching.

--- src/repositories/statement_repository.rs
+++ src/repositories/statement_repository.rs
@@ -3,34 +3,28 @@ use chrono::Local;
 use crate::{
     dto::responses::{balance_response::BalanceResponse, statement_response::StatementResponse},
     model::account::Account,
+    persistence::database::Database,
     repositories::transaction_repository::TransactionRepository,
 };
 
 pub struct StatementRepository;
 
-pub enum StatementRepositoryError {
-    ConnectDatabase,
-    Transaction,
-}
-
 impl StatementRepository {
     pub fn get_statement_by_id(
         &self,
-        id: i32,
+        id: u32,
         account: Account,
-    ) -> Result<StatementResponse, StatementRepositoryError> {
+        db: &mut Database,
+    ) -> StatementResponse {
         let transaction_repository = TransactionRepository;
 
-        match transaction_repository.get_transactions_by_account_id(id) {
-            Ok(transactions) => Ok(StatementResponse {
-                saldo: BalanceResponse {
-                    total: account.saldo,
-                    data_extrato: Local::now(),
-                    limite: account.limite,
-                },
-                ultimas_transacoes: transactions,
-            }),
-            _ => Err(StatementRepositoryError::Transaction),
+        StatementResponse {
+            saldo: BalanceResponse {
+                total: account.saldo,
+                data_extrato: Local::now(),
+                limite: account.limite,
+            },
+            ultimas_transacoes: transaction_repository.get_transactions_by_account_id(id, db),
         }
     }
 }
